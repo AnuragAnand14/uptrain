4749f2b0cd59ebd4532b8c75ff088e53bc4131ad
Your Name <you@example.com>
2024-08-06 17:20:02 +0530

    Deleted unnecasry files

--- uptrain/dashboard/frontend/dashboard/src/components/UI/GraySmallHeading.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const GraySmallHeading = (props) => {
-  return (
-    <h2 className="text-[#5C5C66] font-medium text-base">{props.children}</h2>
-  );
-};
-
-export default GraySmallHeading;
--- uptrain/integrations/__init__.pyi
+++
@@ -1,4 +0,0 @@
-__all__ = ["EvalLlamaIndex", "EvalPromptfoo"]
-
-from .llama_index import EvalLlamaIndex
-from .promptfoo import EvalPromptfoo
